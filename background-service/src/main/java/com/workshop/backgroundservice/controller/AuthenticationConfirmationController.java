0513e6f9c22d777bd5ee47c7643e66bbefe71bb4
DaniilDDDDD <daniilpanyushin@yandex.ru>
2023-03-23 20:06:28 +0300

    0.7.6 application-background deployment CORS error fixed. Global deployment script (deploy.sh) added.

--- background-service/src/main/java/com/workshop/backgroundservice/controller/AuthenticationConfirmationController.java
+++ background-service/src/main/java/com/workshop/backgroundservice/controller/AuthenticationConfirmationController.java
@@ -5,11 +5,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "AuthenticationConfirmation", description = "AuthenticationConfirmation's endpoints")
---
+++ deployment/kubernetes/deploy.sh
@@ -0,0 +1,18 @@
+cd rabbitmq
+bash deploy-rabbitmq.sh
+
+cd ../database/mongo/content
+bash deploy-mongo-content.sh
+cd ../metadata
+bash deploy-mongo-metadata.sh
+cd ../../postgres
+bash deploy-postgres.sh
+
+cd ../../application/auth/deploy-application-auth.sh
+bash deploy-application-auth.sh
+cd ../background
+bash deploy-application-background.sh
+cd ../content
+bash deploy-application-content.sh
+cd ../metadata
+bash deploy-application-metadata.sh
